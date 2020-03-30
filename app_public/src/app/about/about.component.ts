a1bbab0e5a17852448139f40ea94bd5ed63db947
Tina Huang <t42huang@gmail.com>
2020-03-30 19:58:02 -0400

    move data from angular html file into the ts file, create a pipe to parse the html tags in the data

--- app_public/src/app/about/about.component.ts
+++ app_public/src/app/about/about.component.ts
@@ -15,6 +15,7 @@ export class AboutComponent implements OnInit {
   public pageContent = {
     header: {
       title: 'About Wifinder',
-    }
+    },
+    content: "Wifinder was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. Nullam sit amet interdum magna. Morbi quis faucibus nisi. Vestibulum mollis purus quis eros adipiscing tristique. Proin posuere semper tellus, id placerat augue dapibus ornare. Aenean leo metus, tempus in nisl eget, accumsan interdum dui. Pellentesque sollicitudin volutpat ullamcorper."
   };
 }
